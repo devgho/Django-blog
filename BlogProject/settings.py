a4513afc594b5c5c30dcd3194f398482e935cd2b
devgho <devgho@outlook.com>
2021-03-31 10:41:10 +0800

    Fix staticfile import

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'comments'
+    'comments',
+    'gunicorn'
 ]
 
 MIDDLEWARE = [
@@ -124,5 +126,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/"static"]
-STATIC_ROOT = [BASE_DIR/"static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
